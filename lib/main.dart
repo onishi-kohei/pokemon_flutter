2ecff0664a4b1669acc18c51824114749f4de0a0
onishi.kohei <kohei.o2222@gmail.com>
2023-01-09 23:49:40 +0900

    PokeListItem

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,40 @@ class TopPage extends StatelessWidget {
     return Scaffold(
       body: ListView.builder(
         itemCount: 10000,
-        itemBuilder: (context,index) => PikaButton(index: index)
+        itemBuilder: (context,index) => PokeListItem(index: index)
       ),
     );
   }
 }
 
-class PikaButton extends StatelessWidget {
-  const PikaButton({Key? key, required this.index}) : super(key: key);
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
   final int index;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text('pikachu'),
-      onPressed: () => {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.yellow.withOpacity(.5),
+          borderRadius: BorderRadius.circular(10),
+          image: const DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+            ),
+          ),
+        ),
+      ),
+      title: const Text(
+        'Pikachu',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text(
+        '⚡️electric',
+      ),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
         Navigator.of(context).push(
           MaterialPageRoute(
             builder: (BuildContext context) => const PokeDetail(),
@@ -50,3 +70,5 @@ class PikaButton extends StatelessWidget {
     );
   }
 }
+
+
