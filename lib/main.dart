6f5f155d88a71599d54da99336218f0adf7de086
onishi.kohei <kohei.o2222@gmail.com>
2023-01-09 23:51:06 +0900

    darkmode対応

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const TopPage(),
     );
   }
